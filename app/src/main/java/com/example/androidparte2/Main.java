81990e6741ff7cd1056b2a24cc1bf357f6dd3d5c
Guilherme Pereira <pguilherme926@gmail.com>
2023-05-16 09:31:03 +0100

    teste

---
+++ app/src/main/java/com/example/androidparte2/Main.java
@@ -0,0 +1,13 @@
+package com.example.androidparte2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class Main extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
