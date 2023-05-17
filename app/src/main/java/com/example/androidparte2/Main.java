07c42c6d9880a14c1e4f96eda78e49eabb11ed53
Guilherme Pereira <pguilherme926@gmail.com>
2023-05-17 11:35:23 +0100

    teste

--- app/src/main/java/com/example/androidparte2/Main.java
+++ app/src/main/java/com/example/androidparte2/Main.java
@@ -1,17 +1,34 @@
 package com.example.androidparte2;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
-public class Main extends Activity {
+public class Main extends Activity implements View.OnClickListener {
+    TextView txt;
+    Button btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        TextView txt = findViewById(R.id.txt);
-        Button btn = findViewById(R.id.btn); // teste
+        txt = (TextView) findViewById(R.id.txt);
+        btn = (Button) findViewById(R.id.btn);
+
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        txt.setText("Ola via java");
+        Toast.makeText(Main.this, "BODA", Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(this,Main2.class); // (origem,destino)
+        intent.putExtra("nome","sergio");
+        startActivity(intent);
     }
 }
