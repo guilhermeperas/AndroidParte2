2ee5eade7e91e1626708b395a1519fade75985c1
Guilherme Pereira <pguilherme926@gmail.com>
2023-05-17 10:00:16 +0100

    teste

--- app/src/main/java/com/example/androidparte2/Main.java
+++ app/src/main/java/com/example/androidparte2/Main.java
@@ -2,12 +2,16 @@ package com.example.androidparte2;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class Main extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.main);
+        TextView txt = findViewById(R.id.txt);
+        Button btn = findViewById(R.id.btn);
     }
 }
