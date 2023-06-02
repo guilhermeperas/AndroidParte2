295be826f020898c5711d0b6429c9544d7edbda3
Guilherme Pereira <pguilherme926@gmail.com>
2023-06-02 12:46:34 +0100

    DB EXAMPLE COM DETALHE VISIVEL

---
+++ app/src/main/java/com/example/androidparte2/weather/Tempo.java
@@ -0,0 +1,68 @@
+package com.example.androidparte2.weather;
+
+public class Tempo {
+    private int id;
+    private String description;
+    private String icon;
+    private float temp;
+    private float humidade;
+    private float temp_min;
+    private float temp_max;
+    public byte[] iconData; // guarda a imagem em binario
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public float getTemp() {
+        return temp;
+    }
+
+    public void setTemp(float temp) {
+        this.temp = temp;
+    }
+
+    public float getHumidade() {
+        return humidade;
+    }
+
+    public void setHumidade(float humidade) {
+        this.humidade = humidade;
+    }
+
+    public float getTemp_min() {
+        return temp_min;
+    }
+
+    public void setTemp_min(float temp_min) {
+        this.temp_min = temp_min;
+    }
+
+    public float getTemp_max() {
+        return temp_max;
+    }
+
+    public void setTemp_max(float temp_max) {
+        this.temp_max = temp_max;
+    }
+}
